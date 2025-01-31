bfcd953ffeeab0b7d603e6b2875913fa5625ab5f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-31 13:38:24 -0500

    callback is working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,9 +5,10 @@ import { TodoPage } from './todo.page'
 it('creates a todo item', () => {
   TodoPage.reset()
   TodoPage.visit()
-  TodoPage.addTodo('write code')
-  TodoPage.getTodos()
-    .should('have.length', 1)
-    .first()
-    .should('have.attr', 'data-todo-id')
+  TodoPage.addTodo('write code', (id) => {
+    TodoPage.getTodos()
+      .should('have.length', 1)
+      .first()
+      .should('have.attr', 'data-todo-id', id)
+  })
 })
--- cypress/e2e/todo.page.js
+++ cypress/e2e/todo.page.js
@@ -7,12 +7,24 @@ export const TodoPage = {
   /**
    * Adds a new todo item
    * @param {string} title
+   * @param {Function?} callback
    */
-  addTodo(title) {
+  addTodo(title, callback) {
+    cy.intercept('POST', '/todos').as('newTodo')
     cy.get('.new-todo').type(`${title}{enter}`)
+    if (callback) {
+      cy.wait('@newTodo')
+        .its('response.body.id')
+        .should('be.a', 'string')
+        .then(callback)
+    } else {
+      cy.wait('@newTodo')
+        .its('response.body.id')
+        .should('be.a', 'string')
+    }
   },
 
-  resetData() {
+  reset() {
     cy.request('POST', '/reset', { todos: [] })
   },
 
